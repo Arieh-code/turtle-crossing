92ea9fcabb79159e14d96109b3277a1698ad7800
Arieh Norton <arieh.norton@gmail.com>
2022-06-09 21:12:46 +0300

    created scoreboard and logic

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,26 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
+ALIGNMENT = "center"
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(-220, 260)
+        self.level = 1
+        self.start_game()
+
+    def start_game(self):
+        self.write(f"level: {self.level}", align=ALIGNMENT, font=FONT)
+
+    def next_level(self):
+        self.level += 1
+        self.clear()
+        self.write(f"level: {self.level}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER :(", align=ALIGNMENT, font=FONT)
