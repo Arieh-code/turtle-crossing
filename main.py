53df6aec422c90a1249e64073f98e8d27c70005a
Arieh Norton <arieh.norton@gmail.com>
2022-06-09 21:13:22 +0300

    added scoreboard and logic into main

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.move, "Up")
@@ -25,12 +26,12 @@ while game_is_on:
     for car in car_manager.all_cars:
         if player.distance(car) < 25:
             game_is_on = False
-            # print game over
+            scoreboard.game_over()
 
     # detect when reached the end
     if player.ycor() == 290:
         player.next_level()
         car_manager.update_speed()
-        # print next level
+        scoreboard.next_level()
 
 screen.exitonclick()
